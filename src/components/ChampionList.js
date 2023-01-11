a5f7330089362e3ce50e09db3b6b983e645e6e26
ZeckeyBay <zfatihturan@gmail.com>
2023-01-11 16:48:12 +0300

    Lol champ list started

---
+++ src/components/ChampionList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+import SingleChampion from './SingleChampion'
+
+const ChampionList = () => {
+    const [champions,setChampions] = useState({})
+const fetchChampions = async() =>{
+    const response = await axios.get('http://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json')
+    setChampions(response.data.data)
+}
+useEffect(()=>{
+    fetchChampions()
+},[])
+  return (
+    Object.keys(champions).map((x)=>(
+        <div>
+            <h1>{x}</h1>
+        </div>
+    ))
+    
+)}
+
+export default ChampionList
---
+++ src/components/SingleChampion.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleChampion = ({champion}) => {
+  return (
+    <div>{champion}</div>
+  )
+}
+
+export default SingleChampion
